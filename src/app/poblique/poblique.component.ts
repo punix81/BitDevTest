540a6158bfceffdefda9b22f913735a1d1c5f446
punix81 <lopespaulo18@gmail.com>
2024-02-25 12:41:33 +0100

    configure router

--- src/app/poblique/poblique.component.ts
+++ src/app/poblique/poblique.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {ObButtonDirective, ObInputClearDirective} from '@oblique/oblique';
+import {NgIf} from '@angular/common';
 
 @Component({
 	selector: 'app-poblique',
 	standalone: true,
-	imports: [FormsModule, ObButtonDirective, ObInputClearDirective],
+	imports: [FormsModule, ObButtonDirective, ObInputClearDirective, NgIf],
 	templateUrl: './poblique.component.html',
 	styleUrl: './poblique.component.scss'
 })
