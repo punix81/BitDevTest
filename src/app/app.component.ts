73d699ff3cdbc16f0527e7080ae5c6699621ea60
punix81 <lopespaulo18@gmail.com>
2024-02-25 12:54:33 +0100

    configure router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatButton} from '@angular/material/button';
-import {ObButtonDirective} from '@oblique/oblique';
+import {ObButtonDirective, ObMasterLayoutModule} from '@oblique/oblique';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, MatButton, ObButtonDirective],
+	imports: [RouterOutlet, MatButton, ObButtonDirective, ObMasterLayoutModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatButton} from '@angular/material/button';
-import {ObButtonDirective} from '@oblique/oblique';
+import {ObButtonDirective, ObMasterLayoutModule} from '@oblique/oblique';
 import {PobliqueComponent} from '../poblique/poblique.component';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [RouterOutlet, MatButton, ObButtonDirective, PobliqueComponent],
+	imports: [RouterOutlet, MatButton, ObButtonDirective, PobliqueComponent, ObMasterLayoutModule],
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss']
 })
